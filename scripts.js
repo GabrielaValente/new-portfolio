f1944f13d17aefd6a8cf4cb53014660498f62120
Gabriela Valente <contatogabrielav@hotmail.com>
2022-05-12 15:19:05 -0300

    > teste

--- scripts.js
+++ scripts.js
@@ -3,10 +3,16 @@ const cards = document.querySelectorAll(".card")
 
 for (let card of cards) {
     card.addEventListener("click", function () {
+        const projectId = card.getAttribute("id");
         modalOverlay.classList.add("active");
+        modalOverlay.querySelector("iframe").src = `https://kayke-fujinaka.github.io/${projectId}`; // Com os projetos https://gabrielavalente.github.io/
+
+
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function () {
     modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
+
 })
